03d0073d8cd06ed18e1c12e2829025dce2077ced
deepakachu5114 <123865092+deepakachu5114@users.noreply.github.com>
2024-03-10 05:00:21 +0530

    Update image_caption_application.py
    edited code

--- image_caption_application.py
+++ image_caption_application.py
@@ -88,5 +88,5 @@ if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_column_width=True)
         # Make inference
-        captioner = model_loading("/home/deepakachu/Downloads/trained_captioning_model.pth")
+        captioner = model_loading()
         make_inference(image, captioner)
