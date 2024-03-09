389a605acf9a765d37c61fad5754880cf4e35704
raajanwankhade <113465232+raajanwankhade@users.noreply.github.com>
2024-03-10 04:55:21 +0530

    Added all necessary files.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# TRINIT_giant-spoons_ML
-Image Captioning for Remote Sensing Data
+# Image Captioning for Remote Sensing Data
+Team Name : TRINIT_giant-spoons_ML
+
+[Demo Video](https://drive.google.com/file/d/15pppNEg9lJwh0UlrcC6isNwk925E4yTJ/view?usp=sharing)
+
+
+
+## Introduction
+This project involves creation of an application for image captioning remote sensing data (satellite imagery) from the RSICD Dataset.
+
+From urban planning and environmental monitoring to disaster management and agricultural analysis, the applications of remote sensing data are diverse and far-reaching.
+
+## Dataset Description
+The dataset consists of three primary files: train.csv, test.csv, and valid.csv. These files
+contain information about image filenames and their respective captions. Each file includes multiple
+captions for each image to support diverse training techniques.
+
+- `train.csv`: This file contains filenames (filename column) and their corresponding captions
+(captions column) for training your image captioning model.
+- `test.csv`: The test set is included in this file, which contains a similar structure as that of
+train.csv. The purpose of this file is to evaluate your trained models on unseen data.
+- `valid.csv`: This validation set provides images with their respective filenames (filename)
+and captions (captions). It allows you to fine-tune your models based on performance
+during evaluation.
+
+## Evaluation Metric
+**BLEU (Bilingual Evaluation Understudy) Score** - BLEU score provides a quantitative measure of the quality of generated captions compared to reference captions. 
+
+## Model Used
+**GIT (GenerativeImage2Text), base-sized**:
+- GIT (short for GenerativeImage2Text) model, base-sized version. It was introduced in the paper [GIT: A Generative Image-to-text Transformer for Vision and Language](https://arxiv.org/abs/2205.14100) by Wang et al.
+
+### Model description
+- GIT is a Transformer decoder conditioned on both CLIP image tokens and text tokens. The model is trained using "teacher forcing" on a lot of (image, text) pairs.
+- The goal for the model is simply to predict the next text token, giving the image tokens and previous text tokens.
+- The model has full access to (i.e. a bidirectional attention mask is used for) the image patch tokens, but only has access to the previous text tokens (i.e. a causal attention mask is used for the text tokens) when predicting the next text token.
+- We fine-tuned the GIT-Base Model on the RSCID Dataset.
+
+## Application
+The streamlit application involves taking the image as input from the user and getting inference from the model as the generated caption, as seen in the demonstration.
---
+++ image_caption_application.py
@@ -0,0 +1,92 @@
+import streamlit as st
+from PIL import Image
+import torch
+from torchvision import transforms
+import numpy as np
+from transformers import AutoModelForCausalLM, AutoProcessor
+
+def denormalize_pixel_values(pixel_values):
+    """
+    Denormalizes pixel values of an image tensor.
+
+    Args:
+        pixel_values (torch.Tensor): A tensor representing the normalized pixel values of an image.
+                                     Shape: (channels, height, width).
+
+    Returns:
+        PIL.Image.Image: An image with denormalized pixel values.
+
+    Note:
+        This function assumes that the input tensor has been normalized using the same mean
+        and standard deviation values as used during the normalization process.
+    """
+    unnormalized_image = (pixel_values.numpy() * np.array(STD)[:, None, None]) + np.array(MEAN)[:, None, None]
+    unnormalized_image = (unnormalized_image * 255).astype(np.uint8)
+    unnormalized_image = np.moveaxis(unnormalized_image, 0, -1)
+    return Image.fromarray(unnormalized_image)
+
+def model_loading():
+    """
+    Load our fine-tuned image captioning model from HF.
+
+    Returns:
+        Pre-trained image captioning model loaded from the Hugging Face model hub.
+
+    Note:
+        This function loads the image captioning model pre-trained on the RSICD dataset.
+    """
+    model_captioner = AutoModelForCausalLM.from_pretrained("deepakachu/rsicd_image_captioning")
+    model_captioner.to(device)
+    return model_captioner
+
+def make_inference(image, model):
+    """
+    Generate a caption for the given image using the provided image captioning model.
+
+    Args:
+        image (PIL.Image.Image): The input image for which the caption is to be generated.
+        model: Fine-tuned image captioning model.
+
+    Note:
+        This function resizes the input image to (224, 224) before applying the transformation.
+        It then generates a caption using the provided image captioning model and displays the caption.
+    """
+    # resize the image before applying the transformation
+    resized_image = image.resize((224, 224))
+    # apply the transformation
+    torch_tensor = transform(resized_image)
+    # process the single image
+    pixel_values = torch_tensor.unsqueeze(0).to(device)  # Add batch dimension
+    # generate caption
+    generated_ids = model.generate(pixel_values=pixel_values, max_length=100)
+    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    st.markdown(
+        f"<b><p style='font-size:30px; text-align:center; color:#E74C3C;'>Generated Caption: {generated_caption}</p></b>",
+        unsafe_allow_html=True
+    )
+# Load pre-trained model and initialize constants
+processor = AutoProcessor.from_pretrained("microsoft/git-base")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+MEAN = np.array([123.675, 116.280, 103.530]) / 255
+STD = np.array([58.395, 57.120, 57.375]) / 255
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert PIL Image to Tensor
+    transforms.Normalize(mean=MEAN, std=STD)  # Normalize the tensor
+])
+
+# Streamlit app
+st.title("Image Captioning App")
+
+# Upload image through Streamlit
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Make inference when button is clicked
+    if st.button("Generate Caption"):
+        # Open and display the image
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        # Make inference
+        captioner = model_loading("/home/deepakachu/Downloads/trained_captioning_model.pth")
+        make_inference(image, captioner)
